b27473b3a3e804da5cad8b4fae9ed65feadd5f72
Avi <avishkar.22011115@viit.ac.in>
2023-08-08 22:51:04 +0530

    Fixes #9213 feat(nimbus): Added tests for the redesigned sidebar in history page

--- experimenter/experimenter/changelog/tests/test_views.py
+++ experimenter/experimenter/changelog/tests/test_views.py
@@ -38,3 +38,54 @@ class NimbusChangeLogsViewTest(TestCase):
         response = self.view(request, slug=self.experiment.slug)
         context = response.context_data
         self.assertEqual(context["experiment"], self.experiment)
+
+    def test_new_tab_link_rendering(self):
+        request = self.factory.get(
+            reverse("changelogs-by-slug", kwargs={"slug": self.experiment.slug})
+        )
+        request.user = self.user
+        response = self.view(request, slug=self.experiment.slug)
+  
+        content = response.render().content
+
+        buttons = [
+            {
+                "label": "Experimenter Documentation",
+                "redirectLink": "https://experimenter.info/",
+                "iconClass": "fa-solid fa-book",
+            },
+            {
+                "label": "#ask-experimenter",
+                "redirectLink": "https://mozilla.slack.com/?redir=%2Farchives%2FCF94YGE03",
+                "iconClass": "fa-brands fa-slack",
+            },
+            {
+                "label": "Feedback",
+                "redirectLink": "https://mozilla-hub.atlassian.net/secure/CreateIssueDetails!init.jspa?pid=10203&issuetype=10097",
+                "iconClass": "fa-solid fa-message",
+            },
+            {
+                "label": "File targeting criteria request",
+                "redirectLink": "https://github.com/mozilla/experimenter/issues/new",
+                "iconClass": "fa-brands fa-github",
+            },
+        ]
+
+        for button in buttons:
+            self.assertIn(button["label"].encode(), content)
+            self.assertIn(f'href="{button["redirectLink"]}"'.encode(), content)
+            self.assertIn(button["iconClass"].encode(), content)
+
+
+
+    def test_navigation_button_redirect(self):
+        request = self.factory.get(
+            reverse("changelogs-by-slug", kwargs={"slug": self.experiment.slug})
+        )
+        request.user = self.user
+        response = self.view(request, slug=self.experiment.slug)
+
+        content = response.render().content
+
+        expected_link = reverse("nimbus-detail", kwargs={"slug": self.experiment.slug})
+        self.assertIn(f'href="{expected_link}"'.encode(), content)
